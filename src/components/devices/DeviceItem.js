d11fa7f6aba057b7652910f1e57b4287c489fe21
Luis Antonio <antonhy.romero98@gmail.com>
2022-05-02 19:44:06 -0500

    Contador de tiempo funcionando. Los colores del contador cambian dependiendo el estado de la renta.

--- src/components/devices/DeviceItem.js
+++ src/components/devices/DeviceItem.js
@@ -6,21 +6,33 @@ import classes from './DeviceItem.module.css';
 
 function DeviceItem(props) {
   const [rentHours, setRentHours] = useState(0);
-  const [initialTime, setInitialTime] = useState({ isSet: false, hours: 0, minutes: 0 });
-  const [rentTime, setRentTime] = useState();
+  const [initialTime, setInitialTime] = useState({
+    isSet: false,
+    hours: 0,
+    minutes: 0,
+  });
+  const [rentTime, setRentTime] = useState(0);
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-      
-  //   }, 60000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (initialTime.isSet) {
+        setRentTime((previousTime) => previousTime + 1);
+      }
+    }, 1000);
 
-  //   return () => {
-  //     clearInterval(interval);
-  //   }
-  // },[]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [initialTime.isSet]);
 
   const addHoursHandler = () => {
-    setRentHours((prevHrs) => prevHrs + 1);
+    setRentHours((prevHrs) => {
+      if (prevHrs < 99) {
+        return prevHrs + 0.5;
+      } else {
+        return prevHrs;
+      }
+    });
   };
 
   const removeHoursHandler = () => {
@@ -28,14 +40,22 @@ function DeviceItem(props) {
       if (prevHrs === 0) {
         return 0;
       }
-      return prevHrs - 1;
+
+      if (prevHrs <= Math.floor(rentTime / 60)) {
+        alert(
+          'Las horas a rentar no pueden ser menor a las horas de la renta actual.'
+        );
+        return prevHrs;
+      }
+
+      return prevHrs - 0.5;
     });
   };
 
   const startRentHandler = () => {
     const timezoneDiff = new Date().getTimezoneOffset() * 60000;
     const todayDate = new Date().getTime() - timezoneDiff;
-    setRentTime();
+    setRentTime(0);
     setInitialTime({
       isSet: true,
       hours: Math.floor((todayDate / (1000 * 60 * 60)) % 24),
@@ -44,24 +64,26 @@ function DeviceItem(props) {
   };
 
   const stopRentHandler = () => {
-    setInitialTime({isSet: false, hours: 0, minutes: 0 });
+    setRentTime(0);
+    setInitialTime({ isSet: false, hours: 0, minutes: 0 });
+    setRentHours(0);
   };
 
   return (
     <Card className={classes.device}>
       <div className={classes.badge}>
         <img src={device} alt={props.name} />
-        {!initialTime.isSet && <p className={classes.free}>LIBRE</p>}
-        {initialTime.isSet && <p className={classes.occupied}>EN USO</p>}
+        {!initialTime.isSet && <p className={classes.green}>LIBRE</p>}
+        {initialTime.isSet && <p className={classes.red}>EN USO</p>}
       </div>
       <div className={classes.content}>
         <h4>{props.name}</h4>
 
         <div className={classes.input}>
-          Horas Rentadas:<span>{rentHours}</span>
+          Horas a Rentar:<span>{rentHours}</span>
           <div className={classes['input-actions']}>
-            <button onClick={addHoursHandler}>+1</button>
-            <button onClick={removeHoursHandler}>-1</button>
+            <button onClick={addHoursHandler}>+0.5</button>
+            <button onClick={removeHoursHandler}>-0.5</button>
           </div>
         </div>
 
@@ -72,7 +94,9 @@ function DeviceItem(props) {
           {initialTime.isSet && (
             <Fragment>
               <button onClick={stopRentHandler}>Detener Tiempo</button>
-              <button className='red-button'>Cancelar</button>
+              <button className='red-button' onClick={stopRentHandler}>
+                Cancelar
+              </button>
             </Fragment>
           )}
         </div>
@@ -84,7 +108,25 @@ function DeviceItem(props) {
               ? '0' + initialTime.minutes
               : initialTime.minutes}
           </strong>
-          <p className={classes.timer}>1:50 Hrs.</p>
+          <p
+            className={`${classes.timer} ${
+              initialTime.isSet
+                ? rentHours === 0
+                  ? classes.blue
+                  : rentHours < rentTime / 60
+                  ? classes.red
+                  : rentHours - 0.2 < rentTime / 60
+                  ? classes.yellow
+                  : classes.green
+                : classes.gray
+            }`}
+          >
+            {Math.floor(rentTime / 60)}:
+            {Math.floor(rentTime % 60) < 10
+              ? '0' + Math.floor(rentTime % 60)
+              : Math.floor(rentTime % 60)}{' '}
+            Hrs.
+          </p>
         </div>
       </div>
     </Card>
