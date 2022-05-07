1b8c1bf8824cd00730dab823ae53ab4c4ad26f8c
Luis Antonio <antonhy.romero98@gmail.com>
2022-05-07 01:28:12 -0500

    Versión a desplegar. No problemas encontrados por el momento.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store/store';
@@ -11,12 +11,12 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <Layout>
           <App />
         </Layout>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
