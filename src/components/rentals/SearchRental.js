c4f3a91ee8e3ddea5b9580783a6e0b1337a92584
Antonio Aguilera <antonhy.romero98@gmail.com>
2023-03-06 23:34:06 -0600

    Actualizada la paleta de colores de la app y otros detalles detalles ajustados. Font cambiado a Inter.

--- src/components/rentals/SearchRental.js
+++ src/components/rentals/SearchRental.js
@@ -5,27 +5,27 @@ import classes from './SearchRental.module.css';
 
 // Handle the date input
 function SearchRental(props) {
-  const dateInput = useRef();
+	const dateInput = useRef();
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    props.onSubmit(dateInput.current.value);
-  };
+	const submitHandler = (event) => {
+		event.preventDefault();
+		props.onSubmit(dateInput.current.value);
+	};
 
-  return (
-    <form onSubmit={submitHandler} className={classes.search}>
-      <div className={classes.inputBox}>
-        <label htmlFor='search'>Día de la Renta: </label>
-        <input
-          ref={dateInput}
-          id='search'
-          type='date'
-          defaultValue={getDate(new Date())}
-        />
-        <button>Buscar</button>
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={submitHandler} className={classes.search}>
+			<div className={classes.inputBox}>
+				<label htmlFor='search'>Día de la Renta: </label>
+				<input
+					ref={dateInput}
+					id='search'
+					type='date'
+					defaultValue={getDate(new Date())}
+				/>
+				<button>Buscar</button>
+			</div>
+		</form>
+	);
 }
 
 export default SearchRental;
